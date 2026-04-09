c585c84f579df8f16e9576134f0c30dab30432eb
cmsebas2 <sebasrojas29@gmail.com>
2026-04-09 18:35:47 -0500

    fix: redirigir almacenamiento a /tmp y ajustar drivers para entorno Serverless

--- api/index.php
+++ api/index.php
@@ -1,3 +1,9 @@
 <?php
+// Forzar el uso del directorio /tmp permitido por Vercel
+putenv('VIEW_COMPILED_PATH=/tmp');
+putenv('CACHE_STORE=array');
+putenv('CACHE_DRIVER=array');
+putenv('SESSION_DRIVER=cookie');
+putenv('LOG_CHANNEL=stderr');
 
 require __DIR__ . '/../public/index.php';
